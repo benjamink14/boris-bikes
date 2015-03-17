5ba26e31c771483362aa21ea7a6f5c5a89236f84
GJMcGowan <gemcgow@gmail.com>
2015-03-17 11:28:34 +0000

    End of Stage 2

---
+++ spec/feature/public_bike_access_spec.rb
@@ -0,0 +1,11 @@
+require 'capybara/rspec'
+require 'docking_station'
+
+feature 'member of public accesses bike' do
+  scenario 'docking station releases a bike that is not broken' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike).not_to be_broken # expect that bike to respond to the method
+    # 'broken?' with false
+  end
+end
